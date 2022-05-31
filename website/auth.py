bb646dce0b105fe731257a83f275b51e56d58e2e
ryanc2112 <67216780+ryanc2112@users.noreply.github.com>
2022-05-31 20:46:29 +0100

    LogIn and SignUp forms created

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,36 @@
-from flask import Blueprint, render_template
+from xmlrpc.client import boolean
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
     return "<p>Log Out</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template("sign_up.html")
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters', category='error')
+        else:
+            flash('Account Created!', category='success')
+            #add new user
+
+
+
+    return render_template("sign_up.html") 

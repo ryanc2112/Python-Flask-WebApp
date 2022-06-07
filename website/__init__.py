cec9634fe4e01c1662524f065c5f986208c30019
ryanc2112 <67216780+ryanc2112@users.noreply.github.com>
2022-06-07 19:59:18 +0100

    User accounts added, Login/Logout added, Notes add/delete added

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = 'database.db'
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '?S#:w-E?m+,7quZ2WAIG4#.NC2:sjV|*8ar*D,W)K/^nu]CxH&Asnmq'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
 
     from .views import views
     from .auth import auth
@@ -11,5 +23,17 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    create_database(app)
 
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database!')
